b4b02e56595b2e6ce4e3fd3071775eab556ebae1
Mark-Oliver Heger <mark-oliver.heger@de.ibm.com>
2017-09-06 14:46:49 +0200

    Fixes #4

--- com.ibm.streamsx.rabbitmq/impl/java/src/com/ibm/streamsx/rabbitmq/i18n/Messages.java
+++ com.ibm.streamsx.rabbitmq/impl/java/src/com/ibm/streamsx/rabbitmq/i18n/Messages.java
@@ -9,7 +9,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "com.ibm.streamsx.rabbitmq.i18n.CommonMessages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.ibm.streamsx.rabbitmq.i18n.Messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
